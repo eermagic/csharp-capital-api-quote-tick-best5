b4002ff7741f762c4e871db6178207dacaed5d86
Mars <eermagic@yahoo.com.tw>
2022-06-28 17:19:47 +0800

    First Commit

---
+++ Future2/Classes/ComboUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Future2.Classes
+{
+    public class ComboUtil
+    {
+        /// <summary>
+        /// 取得值
+        /// </summary>
+        /// <param name="cbo"></param>
+        /// <returns></returns>
+        public static ComboboxItem GetItem(ComboBox cbo)
+        {
+            ComboboxItem item = new ComboboxItem("", "");
+            if (cbo.SelectedIndex > -1)
+            {
+                item = cbo.Items[cbo.SelectedIndex] as ComboboxItem;
+            }
+            return item;
+        }
+    }
+
+    public class ComboboxItem
+    {
+        public ComboboxItem(string value, string text)
+        {
+            Value = value;
+            Text = text;
+        }
+        public string Value
+        {
+            get;
+            set;
+        }
+        public string Text
+        {
+            get;
+            set;
+        }
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
---
+++ Future2/Classes/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Future2.Classes
+{
+    [Serializable]
+    public class UserProfile
+    {
+        public string CapitalUserId; //群益使用者帳號
+        public string CapitalUserPwd; //群益使用者密碼
+    }
+}
